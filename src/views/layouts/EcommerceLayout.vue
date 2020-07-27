4f544b20d4097c1c06724db0c611e836f2a108c7
anthonyophernandez <anthonyophernandez@gmail.com>
2020-07-27 09:55:14 +0200

    Main related products section

--- src/views/layouts/EcommerceLayout.vue
+++ src/views/layouts/EcommerceLayout.vue
@@ -38,7 +38,7 @@
               <line x1="21" y1="21" x2="15" y2="15" />
             </svg>
           </span>
-          <input class="w-full border rounded-sm pl-10 pr-4 py-2 focus:outline-none focus:border-indigo-500" type="text" placeholder="Search">
+          <input class="w-full border border-gray-400  rounded-sm pl-10 pr-4 py-2 focus:outline-none focus:border-indigo-500 placeholder-gray-600" type="text" placeholder="Search">
         </div>
       </div>
     </header>
@@ -53,11 +53,12 @@
           </svg>
         </button>
       </div>
-      <hr class="my-3">
+      <hr class="my-3 border-gray-400">
       <div class="grid gap-6">
         <div class="flex justify-between" v-for="index in 3" :key="index">
           <div class="flex">
-            <img class="w-20 h-20 bg-indigo-500 rounded">
+            <div class="w-20 h-20 bg-indigo-500 rounded">
+            </div>
             <div class="mx-3">
               <h3 class="text-sm text-gray-600">Product #{{ index }}</h3>
               <div class="flex items-center mt-2">
@@ -85,7 +86,7 @@
       </div>
       <div class="mt-8">
         <form class="flex items-center justify-between" @submit.prevent="">
-          <input class="border rounded-sm w-40 px-4 py-2 focus:outline-none focus:border-indigo-500" type="text" placeholder="Add code">
+          <input class="border border-gray-400 rounded-sm w-40 px-4 py-2 focus:outline-none focus:border-indigo-500 placeholder-gray-600" type="text" placeholder="Add code">
           <button class="bg-indigo-500 hover:bg-indigo-700 text-white py-2 px-4 rounded">APPLY</button>
         </form>
       </div>
@@ -103,13 +104,12 @@
     <main class="my-8">
       <div class="mx-auto px-6">
         <div class="flex items-center justify-center">
-          <div class="w-1/2 max-w-lg h-64">
-            <img class="w-full max-w-lg h-full mx-auto bg-indigo-500 rounded">
+          <div class="w-1/2 max-w-xl h-64 bg-indigo-500 rounded">
           </div>
-          <div class="w-1/2 max-w-lg ml-6">
+          <div class="w-1/2 max-w-xl ml-6">
             <h3 class="text-gray-800 text-lg">PRODUCT #1</h3>
             <span class="text-gray-600">55$</span>
-            <hr class="my-3">
+            <hr class="my-3 border-gray-400">
             <div class="mt-1">
               <span class="text-gray-800 text-sm">Count:</span>
               <div class="flex items-center mt-2">
@@ -156,7 +156,25 @@
           </div>
         </div>
         <div class="mt-10">
-
+          <h3 class="text-2xl text-gray-700">More Products</h3>
+          <div class="grid gap-3 grid-cols-4 mt-6">
+            <div class="w-full rounded-lg shadow-md overflow-hidden" v-for="index in 4" :key="index">
+              <div class="flex items-end justify-end w-full h-64 bg-indigo-500">
+                <button class="mx-2 -mb-4 p-2 bg-indigo-500 hover:bg-indigo-700 focus:outline-none text-white border-2 border-white rounded-full">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 stroke-current icon icon-tabler icon-tabler-shopping-cart" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z"/>
+                    <circle cx="9" cy="19" r="2" />
+                    <circle cx="17" cy="19" r="2" />
+                    <path d="M3 3h2l2 12a3 3 0 0 0 3 2h7a3 3 0 0 0 3 -2l1 -7h-15.2" />
+                  </svg>
+                </button>
+              </div>
+              <div class="px-6 py-4">
+                <h3 class="text-gray-800">PRODUCT #{{ index }}</h3>
+                <span class="text-gray-600">55$</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
